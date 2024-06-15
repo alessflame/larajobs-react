9a61c8c48b29c8a694b4197455af4ee2bc72e34b
alessflame <francescoalessi.code@libero.it>
2024-06-15 09:35:48 +0200

    change app

--- src/helper/Api/Auth/authApi.js
+++ src/helper/Api/Auth/authApi.js
@@ -19,6 +19,7 @@ export const fetchLogout = async () => {
     }
 };
 
+
 export const fetchUpdateUser = async (data) => {
     try {
         let response = await instanceWithToken(getToken()).put(
